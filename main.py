9782252a65b6d4c4d3fc44b236e45767162aa55d
Tariq-Menla <tariqmenla@gmail.com>
2024-09-27 21:23:28 -0400

    Assignment Done

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-numoflabs = float(input("Enter the number of labs completed: "))
-numquizz = float(input("Enter the number of quizzes completed: "))
-numgrA1 = float(input("Enter grade for Assignment 1: "))
-numgrA2 = float(input("Enter grade for Assignment 2: "))
-numgrA3 = float(input("Enter grade for Assignment 3: "))
-numgrA4 = float(input("Enter grade for Assignment 4: "))
-grMT1 = float(input("Enter grade for Midterm 1: "))
-grMT2 = float(input("Enter grade for Midterm 2: "))
-grFE = float(input("Enter grade for Final Exam: "))
-grEprep = float(input("Enter grade for Midterms and Final Preparation: "))
-labgr = ((numoflabs/6)*100*0.2)
-quizzgr = ((numquizz/6)*100*0.15)
-assigr = (((numgrA1 + numgrA2 + numgrA3 + numgrA4)/4)*0.16)
-grMTT = (((grMT1 + grMT2)/2)*0.25)
-grFET = (grFE*0.18)
-grEprepT = (grEprep*0.06)
-Tgrade = (labgr + quizzgr + assigr + grMTT + grFET + grEprepT)
-print("Your Grade is:", round(Tgrade))
+number_of_labs = int(input("Enter the number of labs completed: "))
+if number_of_labs > 6:
+    number_of_labs = 6
+
+number_of_quizzes_andor_inclass_activities = int(input("Enter the number of quizzes completed: "))
+if number_of_quizzes_andor_inclass_activities > 6:
+    number_of_quizzes_andor_inclass_activities = 6
+
+Assignment1_grade = float(input("Enter grade for Assignment 1: "))
+Assignment2_grade = float(input("Enter grade for Assignment 2: "))
+Assignment3_grade = float(input("Enter grade for Assignment 3: "))
+Assignment4_grade = float(input("Enter grade for Assignment 4: "))
+
+Midterm1_grade = float(input("Enter grade for Midterm 1: "))
+Midterm2_grade = float(input("Enter grade for Midterm 2: "))
+FinalExam_grade = float(input("Enter grade for Final Exam: "))
+Midterm_and_Final_prep = float(input("Enter grade for Midterms and Final Preparation: "))
+
+labs_weighted_grade = ((number_of_labs/6)*100*0.2)
+quizzes_andor_inclass_weighted_grade = ((number_of_quizzes_andor_inclass_activities/6)*100*0.15)
+weighted_assignments_grade = (((Assignment1_grade + Assignment2_grade + Assignment3_grade + Assignment4_grade)/4)*0.16)
+weighted_midterms_grade = (((Midterm1_grade + Midterm2_grade)/2)*0.25)
+weighted_finalexam_grade = (FinalExam_grade*0.18)
+weighted_examprep_for_midterm_and_final = (Midterm_and_Final_prep*0.06)
+
+Total_grade = (labs_weighted_grade + quizzes_andor_inclass_weighted_grade + weighted_assignments_grade + weighted_midterms_grade + weighted_finalexam_grade + weighted_examprep_for_midterm_and_final)
+print("Your Grade is:", round(Total_grade,2), "%")
+
